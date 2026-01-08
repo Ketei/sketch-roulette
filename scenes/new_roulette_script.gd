78cb2bf00b800158958d604fd38f3e35d75db4b8
Ketei <ketei.ilust@gmail.com>
2026-01-07 23:23:16 -0600

    Cleanup

--- scenes/new_roulette_script.gd
+++ scenes/new_roulette_script.gd
@@ -1136,15 +1136,14 @@ func _on_preset_saved(preset_id: int, preset_name: String, format_template: Stri
 			var triggers: Array[int] = active_preset.get_trigger_groups(group_id, tag_id)
 			var new_rows: Array[Dictionary] = []
 			for target_group_id in triggers:
+				var values: String = str("(", tag_id, ",", group_id, ",", target_group_id, ")")
+				
 				database.insert_row(
 						"tag_triggers_groups",
 						{
 						"tag_id": tag_id,
 						"group_id": group_id,
 						"triggers_group": target_group_id})
-			
-			if not new_rows.is_empty():
-				success = database.insert_rows("tag_triggers_groups", new_rows)
 			
 			if not success:
 				abort_msg = "Tag Triggers Table"
